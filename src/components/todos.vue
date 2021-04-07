1793ec25617199f3c8302d7ba912bbc29d622e80
Cedric AUDRIT <cedric@audrit.be>
2021-04-07 13:13:10 +0200

    finish functionnal todo list

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -11,55 +11,36 @@
       />
     </header>
     <div class="main">
+      <input type="checkbox" class="toggle-all" v-model="allDone">
       <ul class="todo-list">
         <li
           class="todo"
           v-for="todo in filteredTodos"
-          :class="{ completed: todo.completed }"
-          v-bind:key="todo"
-        >
+          :class="{ completed: todo.completed , editing: todo === editing}">
           <div class="view">
             <input type="checkbox" v-model="todo.completed" class="toggle"/>
-            <label>{{ todo.name }}</label>
+            <label @dblclick="editTodo(todo)">{{ todo.name }}</label>
+            <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
           </div>
+          <input type="text" class="edit" v-model="todo.name" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus="todo === editing">
         </li>
       </ul>
     </div>
-    <footer class="footer">
-      <span class="todo-count"
-      ><strong>{{ remaining }}</strong> Task todo</span
-      >
+    <footer class="footer" v-show="hasTodo">
+      <span class="todo-count"><strong>{{ remaining }}</strong> Task todo</span>
       <ul class="filters">
-        <li>
-          <a
-            href="#"
-            :class="{ selected: filter === 'all' }"
-            @click.prevent="filter = 'all'"
-          >All</a
-          >
-        </li>
-        <li>
-          <a
-            href="#"
-            :class="{ selected: filter === 'todo' }"
-            @click.prevent="filter = 'todo'"
-          >To Do</a
-          >
-        </li>
-        <li>
-          <a
-            href="#"
-            :class="{ selected: filter === 'done' }"
-            @click.prevent="filter = 'done'"
-          >Done</a
-          >
-        </li>
+        <li><a href="#" :class="{ selected: filter === 'all' }" @click.prevent="filter = 'all'">All</a></li>
+        <li><a href="#" :class="{ selected: filter === 'todo' }" @click.prevent="filter = 'todo'">To Do</a></li>
+        <li><a href="#" :class="{ selected: filter === 'done' }" @click.prevent="filter = 'done'">Done</a></li>
       </ul>
+      <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted">Delete all Done</button>
     </footer>
   </section>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   data () {
     return {
@@ -70,7 +51,9 @@ export default {
         }
       ],
       newTodo: '',
-      filter: 'all'
+      filter: 'all',
+      editing: null,
+      oldTodos: ''
     }
   },
   methods: {
@@ -80,19 +63,63 @@ export default {
         name: this.newTodo
       })
       this.newTodo = ''
+    },
+    deleteTodo (todo) {
+      this.todos = this.todos.filter(i => i !== todo)
+    },
+    deleteCompleted () {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    },
+    editTodo (todo) {
+      this.editing = todo
+      this.oldTodos = todo.name
+    },
+    doneEdit () {
+      this.editing = null
+    },
+    cancelEdit () {
+      this.editing.name = this.oldTodos,
+      this.doneEdit()
     }
   },
   computed: {
+    allDone: {
+      get (){
+        return this.remaining === 0
+      },
+      set (value) {
+        this.todos.forEach(todo => {
+          todo.completed = value
+        })
+
+      }
+    },
     remaining () {
       return this.todos.filter(todo => !todo.completed).length
     },
+    completed () {
+      return this.todos.filter(todo => todo.completed).length
+    },
+    hasTodo () {
+      return this.todos.length > 0
+    },
     filteredTodos () {
       if (this.filter === 'todo') {
-        return this.todos(todo => !todo.completed)
+        return this.todos.filter(todo => !todo.completed)
       } else if (this.filter === 'done') {
-        return this.todos(todo => todo.completed)
+        return this.todos.filter(todo => todo.completed)
       }
       return this.todos
+    }
+  },
+  directives: {
+    focus (el, value) {
+      if (value) {
+        Vue.nextTick(_ => {
+          el.focus()
+        })
+      }
+
     }
   }
 }
